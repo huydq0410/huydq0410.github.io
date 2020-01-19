f8e1df500a5adcb836748c60da17f1e6d13a5f6a
Do Quang Huy <huydo0410@gmail.com>
2020-01-19 21:51:33 +0700

    Updates

---
+++ precache-manifest.ce9e416a1fbb9c9d1c8e7290015f3302.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd5e6aa69f00fe8003f5f8d01c431c8",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "a69c58f63b1d97dc68b4",
+    "url": "/mywebsite/static/css/main.8c24f3fa.chunk.css"
+  },
+  {
+    "revision": "b1903ce414c92c4981c6",
+    "url": "/mywebsite/static/js/2.415d925f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/mywebsite/static/js/2.415d925f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a69c58f63b1d97dc68b4",
+    "url": "/mywebsite/static/js/main.ba211ea4.chunk.js"
+  },
+  {
+    "revision": "ffc7faa699f29447f04f",
+    "url": "/mywebsite/static/js/runtime-main.d4259911.js"
+  },
+  {
+    "revision": "d6079ef01292c4bc84dce33988641530",
+    "url": "/mywebsite/static/media/SFProText-Bold.d6079ef0.ttf"
+  },
+  {
+    "revision": "a260cbc18870da144038776461d9df28",
+    "url": "/mywebsite/static/media/SFProText-Medium.a260cbc1.ttf"
+  },
+  {
+    "revision": "85bd46c1cff02c1d8360cc714b8298fa",
+    "url": "/mywebsite/static/media/SFProText-Regular.85bd46c1.ttf"
+  },
+  {
+    "revision": "1a131c948d598ecec700d37d168a15b5",
+    "url": "/mywebsite/static/media/SFProText-Semibold.1a131c94.ttf"
+  },
+  {
+    "revision": "16c5260cedb70b3056516d16a738317e",
+    "url": "/mywebsite/static/media/backgroundtop.16c5260c.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mywebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
